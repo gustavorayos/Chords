3a8e09b59dc14d51ae53acef63edbfa3d5d4f535
Gustavo Rayos <gustavorayos@outlook.com>
2025-07-31 17:16:37 -0700

    Update shellchords.ps1

--- Shellchords/shellchords.ps1
+++ Shellchords/shellchords.ps1
@@ -1,5 +1,4 @@
 # PowerShell script to calculate diatonic chords and triads
-#($ctx = $executioncontext.gettype().getfield("_context","nonpublic,instance").getvalue( $executioncontext)).gettype().getfield("_authorizationManager","nonpublic,instance").setvalue($ctx, (new-object System.Management.Automation.AuthorizationManager "Microsoft.PowerShell"))
 
 function centerText ($string1) {
 
